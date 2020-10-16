05d7284ee1c7729831ca44fa6e63b48a0b12d61a
yoshiki0214 <yoshikishima0214m@gmail.com>
2020-10-16 16:10:40 +0900

    commit message

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,12 @@ return [
     */
 
     'paths' => ['*'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => ['*'],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => false,
-
     'max_age' => false,
-
     'supports_credentials' => false,
 
 ];
